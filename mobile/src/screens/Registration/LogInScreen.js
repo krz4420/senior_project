fc8850a3ab357486de49aa1c6c495263726cd604
Kyle Zawadzki <kylezawadzki@Kyles-MacBook-Pro.local>
2022-11-06 18:41:02 -0500

    Linting and styling with prettier. It auto formatted all the files and white space

--- mobile/src/screens/Registration/LogInScreen.js
+++ mobile/src/screens/Registration/LogInScreen.js
@@ -1,146 +1,153 @@
-import React, { useState } from 'react'
-import { ScrollView, View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native'
-import Logo from '../../../assets/icon.png'
-import CustomInput from '../../components/CustomInput'
-import CustomButton from '../../components/CustomButton'
-import { useNavigation } from '@react-navigation/native'
-import { useAuth } from '../../context/Auth';
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
+import Logo from "../../../assets/icon.png";
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomButton";
+import { useNavigation } from "@react-navigation/native";
+import { useAuth } from "../../context/Auth";
 
 const LogInScreen = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    
-    const [ validForm, setValidForm ] = useState(true);
-    const auth = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const onLogInPressed =  () => {
+  const [validForm, setValidForm] = useState(true);
+  const auth = useAuth();
 
-        if(isFormEmpty()){
-            console.error("Form is empty")
-            setValidForm(false);
-            return;
-        }
-
-        // Extract user info from text input fields
-        const userInfo = {username, password};
-
-         // TODO. Scrub data to protect against attacks
-
-        auth.signIn(userInfo).then(res => {
-            console.log("Successful login")
-            console.log(res)
-        }).catch(err =>{
-            console.error("Error")
-            console.error(err);
-            // error = err.response.data ? err.response.data.message : err.response;
-            
-            // Set state for valid form to false to render error message
-            setValidForm(false);
-        });
-
-    }
-    const isFormEmpty = () => {
-        const userData = [username, password];
-        console.log(username)
-        if(Object.values(userData).every(val => val.length == 0)){
-            return true;
-        }else{
-            return false;
-        }
-    };
-
-    const clearState = () => {
-        setUsername('');
-        setPassword('');
-        setValidForm(true)
+  const onLogInPressed = () => {
+    if (isFormEmpty()) {
+      console.error("Form is empty");
+      setValidForm(false);
+      return;
     }
 
-    const onForgotPasswordPressed = () => {
+    // Extract user info from text input fields
+    const userInfo = { username, password };
 
-        clearState();
-        navigation.navigate('Forgot Password');
+    // TODO. Scrub data to protect against attacks
+
+    auth
+      .signIn(userInfo)
+      .then((res) => {
+        console.log("Successful login");
+      })
+      .catch((err) => {
+        console.error("Error");
+        console.error(err);
+        // error = err.response.data ? err.response.data.message : err.response;
+
+        // Set state for valid form to false to render error message
+        setValidForm(false);
+      });
+  };
+  const isFormEmpty = () => {
+    const userData = [username, password];
+    if (Object.values(userData).every((val) => val.length == 0)) {
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    // const onGoogleSignIn = () => {
-    //     console.warn("Google Sign In attemp")
-    // }
+  const clearState = () => {
+    setUsername("");
+    setPassword("");
+    setValidForm(true);
+  };
 
-    const onSignUp = () => {
-        console.warn("Sign Up");
+  const onForgotPasswordPressed = () => {
+    clearState();
+    navigation.navigate("Forgot Password");
+  };
 
-        clearState();
-        navigation.navigate('Sign Up');
-    }
+  // const onGoogleSignIn = () => {
+  //     console.warn("Google Sign In attemp")
+  // }
 
-    const { height } = useWindowDimensions();
-    return (
-        <ScrollView showsVerticalScrollIndicator = { false }>
-            <View style={styles.root}>
-                <Image 
-                    source = {Logo} 
-                    style ={[styles.logo, {height: height * 0.3}]} 
-                    resizeMode="contain"/>
+  const onSignUp = () => {
+    console.warn("Sign Up");
 
-                {validForm ? null: 
-                    <Text style={styles.error}>Invalid Login Credentials!</Text>}
+    clearState();
+    navigation.navigate("Sign Up");
+  };
 
-                <CustomInput 
-                    placeholder = "Username"  
-                    value={ username} 
-                    setValue= {setUsername}
-                    autoCapitalize= {false}
-                    onFocus = {setValidForm}
-                    />
-                <CustomInput 
-                    placeholder = "Password" 
-                    value = {password} 
-                    setValue = {setPassword} 
-                    secureTextEntry
-                    autoCapitalize={false}
-                    onFocus = {setValidForm}
-                    />
-                <CustomButton 
-                    text= "Log In" 
-                    onPress= {onLogInPressed} />
+  const { height } = useWindowDimensions();
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, { height: height * 0.3 }]}
+          resizeMode="contain"
+        />
 
-                <CustomButton 
-                    text= "Forgot Password?" 
-                    onPress= {onForgotPasswordPressed} 
-                    type="TERTIARY"/>
+        {validForm ? null : (
+          <Text style={styles.error}>Invalid Login Credentials!</Text>
+        )}
 
-                {/* <CustomButton 
+        <CustomInput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+          autoCapitalize={false}
+          onFocus={setValidForm}
+        />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+          autoCapitalize={false}
+          onFocus={setValidForm}
+        />
+        <CustomButton text="Log In" onPress={onLogInPressed} />
+
+        <CustomButton
+          text="Forgot Password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
+
+        {/* <CustomButton 
                     text= "Sign In with Google" 
                     onPress= {onGoogleSignIn} 
                     type="TERTIARY" 
                     bgColor='#FAE9EA' 
                     fgColor='#DD4D44'/> */}
 
-                <CustomButton 
-                    text= "Don't have an account? Sign up!" 
-                    onPress= {onSignUp} 
-                    type="TERTIARY" />
-            </View>
-        </ScrollView>
-    )
+        <CustomButton
+          text="Don't have an account? Sign up!"
+          onPress={onSignUp}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    root:{
-        alignItems:'center',
-        padding: 20,
-        backgroundColor:'rgba(10, 39, 103, 0.2)'
-    },
-    logo:{
-        width:'30%',
-        maxWidth: 500,
-        maxHeight: 200,
-    },
-    error:{
-        color:'red',
-        borderColor:'red',
-    }
+  root: {
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "rgba(10, 39, 103, 0.2)",
+  },
+  logo: {
+    width: "30%",
+    maxWidth: 500,
+    maxHeight: 200,
+  },
+  error: {
+    color: "red",
+    borderColor: "red",
+  },
 });
 
 export default LogInScreen;
